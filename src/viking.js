577786bbbc2c2fa179eff23afae8c1b2579a2b55
ericruga <ericruga@users.noreply.github.com>
2022-06-07 08:49:20 +0200

    Tercera version del documento

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,33 @@
 // Soldier
-class Soldier {}
+class Soldier {
+
+  // No entiendo porque no se me printea verde en la consola
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+attack(){
+  return this.strength;
+}
+
+receiveDamage(damage){
+  this.health = this.health - damage; // this.health -= damage 
+}
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+constructor(name, health, strength){
+  super(health,strength);
+  this.name = name;
+}
+
+receivedDamage(){}
+
+battleCry(){}
+
+}
 
 // Saxon
 class Saxon {}
