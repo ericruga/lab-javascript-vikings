cdd656aae2303c8f2c133dd1cb0372ebfc09ccc3
ericruga <ericruga@users.noreply.github.com>
2022-06-08 06:56:39 +0200

    Versión ejercicio 4

--- src/viking.js
+++ src/viking.js
@@ -21,22 +21,35 @@ class Viking extends Soldier {
 constructor(name, health, strength){
   super(health,strength);
   this.name = name;
-}
-
-receivedDamage(){
-  this.health = this,.health - damage; //this.health -= damage
-  if(this.health >= 0){
-    return `${this.name} has died in act!`
   }
-  return `${this.name} has received ${}`
+
+
+receiveDamage(damage){
+  this.health = this.health - damage; //this.health -= damage
+  if(this.health >= 0){
+    return `${this.name} has died in act of combat`;
+  } else { return `${this.name} has received ${damage} points of damage`; }
+}
 }
 
-battleCry(){}
-
+battleCry(){
+  return "Odin Owns You All!";
 }
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage){
+    this.health = this.health - damage;
+    if(this.health > 0){
+      return `A saxon has received ${damage} points of damage`;
+      } else { return `A saxon has died in combat`; }
+      
+  attack(){
+    return this.strength;
+      }
+  }
+}
 
 // War
 class War {}
